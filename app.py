38c1106a4be9c7488d86f8ac4a5e6ce2c6ec9dab
tegar-ganang <tegarsatrio10@gmail.com>
2024-06-05 13:22:11 +0700

    update

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, request, jsonify
 import joblib
 import numpy as np
+import os
 
 app = Flask(__name__)
 
 # Load the models
-ph_model = joblib.load('ph_model.pkl')
-tds_model = joblib.load('tds_model.pkl')
+ph_model = joblib.load('random_forest_ph_model.pkl')
+tds_model = joblib.load('random_forest_tds_model.pkl')
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -31,4 +32,5 @@ def predict():
     return jsonify(results)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
